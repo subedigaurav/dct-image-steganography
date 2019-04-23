a768f75773ca0dd27270097146d38e0d657b0a24
subedigaurav <ergauravsubedi@gmail.com>
2019-04-23 07:09:52 +0545

    changed main program!

--- src/backend/MainProgram.java
+++ src/backend/MainProgram.java
@@ -1,5 +1,11 @@
 package backend;
 
 public class MainProgram {
+	private static String coverImage;
+	private static String textFile;
 
+	public static void main(String[] args) {
+		coverImage = args[0];
+		textFile = args[1];
+	}
 }
