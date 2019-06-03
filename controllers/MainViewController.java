7146ec7f9b2408eb4b4ce624d29b2b9a7001ca10
subedigaurav <ergauravsubedi@gmail.com>
2019-06-03 16:01:11 +0545

    Updated readme

--- controllers/MainViewController.java
+++ controllers/MainViewController.java
@@ -20,65 +20,64 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
- 
+
 public class MainViewController implements Initializable {
-  @FXML
-   private Button dateButton;
-  
-   @FXML
-   private Button exitBtn;
-   
-   @FXML
-   private Button encodeBtn;
-   
-   @FXML
-   private TextField myTextField;
-   
-   @FXML
-   private ImageView fbImg;
-  
-   @FXML
-   private ImageView gitImg;
-   
-   @FXML
-   private Button decodeBtn;
-   
-   private ColorAdjust cs;
-   
-   @FXML
-   private Text marqueeText;
-   
-   @FXML
-   private AnchorPane rootPane;
-   
-   @Override
-   public void initialize(URL location, ResourceBundle resources) {
-	   cs=new ColorAdjust();
-	   cs.setBrightness(1);
-	   gitImg.setEffect(cs);
-	   fbImg.setEffect(cs);
-   }
- 
-   private Effect eff;
+    @FXML
+    private Button dateButton;
 
-   private void throwWindow(String fxmlFile, ActionEvent event) throws IOException{
-		//This Gets The Scene Info
-		Stage window=(Stage) ((Node) event.getSource()).getScene().getWindow();
+    @FXML
+    private Button exitBtn;
 
-		Parent coverSelectionStage=FXMLLoader.load(getClass().getResource(fxmlFile));
-		Scene coverSelectionScene=new Scene(coverSelectionStage);
+    @FXML
+    private Button encodeBtn;
 
-		coverSelectionScene.setFill(Color.TRANSPARENT);
-		window.setScene(coverSelectionScene);
-		window.centerOnScreen();
-		window.show();
-	}
+    @FXML
+    private TextField myTextField;
 
-   @FXML
-   public void gotoCoverSelection(ActionEvent event) throws IOException {
-	   throwWindow("/fxml/coverImageSelection.fxml", event);
-	}
-   
+    @FXML
+    private ImageView fbImg;
+
+    @FXML
+    private ImageView gitImg;
+
+    @FXML
+    private Button decodeBtn;
+
+    private ColorAdjust cs;
+
+    @FXML
+    private Text marqueeText;
+
+    @FXML
+    private AnchorPane rootPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        cs = new ColorAdjust();
+        cs.setBrightness(1);
+        gitImg.setEffect(cs);
+        fbImg.setEffect(cs);
+    }
+
+    private Effect eff;
+
+    private void throwWindow(String fxmlFile, ActionEvent event) throws IOException {
+        Parent coverSelectionStage = FXMLLoader.load(getClass().getResource(fxmlFile));
+        Scene coverSelectionScene = new Scene(coverSelectionStage);
+
+        //This Gets The Scene Info
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        coverSelectionScene.setFill(Color.TRANSPARENT);
+        window.setScene(coverSelectionScene);
+        window.centerOnScreen();
+        window.show();
+    }
+
+    @FXML
+    public void gotoCoverSelection(ActionEvent event) throws IOException {
+        throwWindow("/fxml/coverImageSelection.fxml", event);
+    }
+
 //   @FXML
 //   public void gotoDecoderView(ActionEvent event) throws IOException {
 //		Parent decoderParent=FXMLLoader.load(getClass().getResource("decoderView.fxml"));
@@ -91,84 +90,82 @@ public class MainViewController implements Initializable {
 //		window.centerOnScreen();
 //		window.show();
 //	}
-   
-   @FXML
-   public void gotoStegoSelection(ActionEvent event) throws IOException {
-		Parent decoderParent=FXMLLoader.load(getClass().getResource("/fxml/stegoImageSelection.fxml"));
-		Scene decoderScene=new Scene(decoderParent);
-		
-		//This Gets The Scene Info
-		Stage window=(Stage) ((Node) event.getSource()).getScene().getWindow();
-		decoderScene.setFill(Color.TRANSPARENT);
-		window.setScene(decoderScene);
-		window.centerOnScreen();
-		window.show();
-	}
-   
-   // When user click on myButton
-   // this method will be called.
-   public void exitProgram(ActionEvent event) {
-      System.exit(0);
-   }
-   
-   @FXML
-   public void btnChangeFB() {
-	   Image image= new Image(getClass().getResourceAsStream("/images/facebook.png"));
-	   eff=fbImg.getEffect();
-	   ColorAdjust cs=new ColorAdjust();
-	   cs.setSaturation(1);
-	   cs.setContrast(0.70);
-	   fbImg.setEffect(cs);
-	   fbImg.setImage(image);
-   }
-   
-   @FXML
-   public void btnChangeResetFB() {
-	   Image image= new Image(getClass().getResourceAsStream("/images/facebook.png"));
-	   fbImg.setEffect(eff);
-	   fbImg.setImage(image);
-   }
-   
-   @FXML
-   public void openFBPage() {
-	        try { 
-	         String url = "https://www.facebook.com";
-	         java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
-	       }
-	       catch (java.io.IOException e) {
-	           System.out.println(e.getMessage());
-	       }
-   }
-   
-   
-   //Controls for GitHub Button
-   @FXML
-   public void btnChangeGIT() {
-	   Image image= new Image(getClass().getResourceAsStream("/github.png"));
-	   eff=gitImg.getEffect();
-	   ColorAdjust cs=new ColorAdjust();
-	   cs.setSaturation(1);
-	   cs.setContrast(.85);
-	   gitImg.setEffect(cs);
-	   gitImg.setImage(image);
-   }
-   
-   @FXML
-   public void btnChangeResetGIT() {
-	   Image image= new Image(getClass().getResourceAsStream("/images/github-logo.png"));
-	   gitImg.setEffect(eff);
-	   gitImg.setImage(image);
-   }
-   
-   @FXML
-   public void openGITPage() {
-	        try { 
-	         String url = "https://github.com/subedigaurav/DCT-LSB-Image-Steganography";
-	         java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
-	       }
-	       catch (java.io.IOException e) {
-	           System.out.println(e.getMessage());
-	       }
-   }
-   
+
+    @FXML
+    public void gotoStegoSelection(ActionEvent event) throws IOException {
+        Parent decoderParent = FXMLLoader.load(getClass().getResource("/fxml/stegoImageSelection.fxml"));
+        Scene decoderScene = new Scene(decoderParent);
+
+        //This Gets The Scene Info
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        decoderScene.setFill(Color.TRANSPARENT);
+        window.setScene(decoderScene);
+        window.centerOnScreen();
+        window.show();
+    }
+
+    // When user click on myButton
+    // this method will be called.
+    public void exitProgram(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    public void btnChangeFB() {
+        Image image = new Image(getClass().getResourceAsStream("/images/facebook.png"));
+        eff = fbImg.getEffect();
+        ColorAdjust cs = new ColorAdjust();
+        cs.setSaturation(1);
+        cs.setContrast(0.70);
+        fbImg.setEffect(cs);
+        fbImg.setImage(image);
+    }
+
+    @FXML
+    public void btnChangeResetFB() {
+        Image image = new Image(getClass().getResourceAsStream("/images/facebook.png"));
+        fbImg.setEffect(eff);
+        fbImg.setImage(image);
+    }
+
+    @FXML
+    public void openFBPage() {
+        try {
+            String url = "https://www.facebook.com";
+            java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+        } catch (java.io.IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    //Controls for GitHub Button
+    @FXML
+    public void btnChangeGIT() {
+        Image image = new Image(getClass().getResourceAsStream("/images/github.png"));
+        eff = gitImg.getEffect();
+        ColorAdjust cs = new ColorAdjust();
+        cs.setSaturation(1);
+        cs.setContrast(.85);
+        gitImg.setEffect(cs);
+        gitImg.setImage(image);
+    }
+
+    @FXML
+    public void btnChangeResetGIT() {
+        Image image = new Image(getClass().getResourceAsStream("/images/github-logo.png"));
+        gitImg.setEffect(eff);
+        gitImg.setImage(image);
+    }
+
+    @FXML
+    public void openGITPage() {
+        try {
+            String url = "https://github.com/subedigaurav/DCT-LSB-Image-Steganography";
+            java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+        } catch (java.io.IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,23 @@
-Hello to my New Project!
+#DCT LSB Image Steganography Project
+It is a full-fledged JavaFX Application for hiding messages (payload) in a JPEG image.
+
+##Installing
+###Pre-requisites
+1. <b>MySQL Server</b>
+ 
+    The confuguration of MySQL DB is:
+    ```mysql
+    server: localhost
+    port: 3306
+    username: root
+    password: none
+    ```
+    You can use the above credentials, or use you own.
+    Modify the *DbUtil.java* file as necessary. The file is located in src->util
+    
+2. **Java Development Kit 8**
+
+    You can download the JDK from [here](https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html)
+    
+Launch the Application by running *MainWindow.java* located in the src folder.
+###Have a Nice Day!
